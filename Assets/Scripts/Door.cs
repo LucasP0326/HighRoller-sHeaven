ffde8f8cf3b744beda5ce915544bb0919d6657c4
LucasP0326 <2002lucassanchez@gmail.com>
2024-03-25 20:35:00 -0400

    Quick Minor Fix
    Fixed

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -23,7 +23,7 @@ public class Door : MonoBehaviour
     void OnMouseDown()
     {
         // Check if the camera switcher script is assigned
-        if (casinoNavigator != null)
+        if (casinoNavigator != null && casinoNavigator.inConversation == false)
         {
             source.Play(); // This just adds the sound effect.
             // Call the camera switch function with the specified target camera index
